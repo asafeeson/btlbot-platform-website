1ed38eb4d92dcca94bf3d738620455862c4a51b0
asafeeson <g.kochergin@hotmail.com>
2025-04-27 23:42:03 +0200

    - new component

---
+++ src/lib/components/atoms/H3.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	let {
+        color,
+		size = 'text-2xl',
+        font = 'font-bold', 
+		children
+	}: { size?: string, font?: string, color?: string; children: Snippet } = $props();
+</script>
+
+<h3 class="{font} {size} {color}">{@render children()}</h3>
