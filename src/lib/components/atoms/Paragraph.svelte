2e371e243efe2684c84e2b2963f01a2d3faf689c
asafeeson <g.kochergin@hotmail.com>
2025-04-27 21:01:44 +0200

    - init

---
+++ src/lib/components/atoms/Paragraph.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	let { children, extraClass }: { children: Snippet, extraClass?: string } = $props();
+</script>
+
+<p class="w-fit text-xl {extraClass}">
+	{@render children()}
+</p>
