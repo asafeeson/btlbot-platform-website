10d69d11ed5447622a71fcbb0b10b3b7f1105d59
asafeeson <g.kochergin@hotmail.com>
2025-04-27 22:00:39 +0200

    refactor: enhance ButtonLink styling and transition effects

--- src/lib/components/atoms/ButtonLink.svelte
+++ src/lib/components/atoms/ButtonLink.svelte
@@ -2,12 +2,11 @@
 	import type { Snippet } from 'svelte';
 
 	let { url = '/', children }: { url: string; children: Snippet } = $props();
-	
 </script>
 
 <a
 	href={url}
-	class="border-2 border-white px-5 py-4 transition-colors hover:bg-amber-500 hover:text-black"
+	class="rounded-xl border-2 border-white px-5 py-4 transition-all hover:bg-amber-500 hover:text-black hover:shadow-2xl w-fit"
 >
 	{@render children()}
 </a>
