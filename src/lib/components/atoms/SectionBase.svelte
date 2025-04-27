a027dcfa44ac3e1803b6a76387c4dbeab9835f6f
asafeeson <g.kochergin@hotmail.com>
2025-04-27 22:22:24 +0200

    refactor: add innerExtraClass prop to SectionBase component for enhanced styling

--- src/lib/components/atoms/SectionBase.svelte
+++ src/lib/components/atoms/SectionBase.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 
-	let { children, background }: { children: Snippet; background?: string } = $props();
+	let { children, background, innerExtraClass }: { children: Snippet; background?: string, innerExtraClass?: string } = $props();
 </script>
 
 <section
 	class="min-h-xl {background} p-14 lg:px-20"
 >
 	<div
-		class="flex flex-wrap items-start justify-between gap-10 rounded-4xl border border-gray-200 px-10 py-14 shadow-lg transition-shadow hover:shadow-2xl"
+		class="flex flex-wrap items-start justify-between gap-10 rounded-4xl border border-gray-200 px-10 py-14 shadow-lg transition-shadow hover:shadow-2xl {innerExtraClass}"
 	>
 		{@render children()}
 	</div>
