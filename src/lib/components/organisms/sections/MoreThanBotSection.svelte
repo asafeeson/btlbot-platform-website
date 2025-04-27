098d81f9a7357adbc76d48f1ec6adb223cc9a018
asafeeson <g.kochergin@hotmail.com>
2025-04-27 21:05:54 +0200

    - use Paragraph component instead of p tag

--- src/lib/components/organisms/sections/MoreThanBotSection.svelte
+++ src/lib/components/organisms/sections/MoreThanBotSection.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
 	import SectionBase from '$lib/components/atoms/SectionBase.svelte';
 	import H2HugeColored from '$lib/components/atoms/H2HugeColored.svelte';
+	import Paragraph from '$lib/components/atoms/Paragraph.svelte';
 </script>
 
 <SectionBase background="bg-amber-50">
 	<div class="flex flex-wrap items-center justify-between gap-20 lg:flex-nowrap">
 		<H2HugeColored>больше чем просто чат-бот</H2HugeColored>
-		<p class="w-fit text-xl">
+		<Paragraph>
 			BTLbot — это AI-платформа нового поколения, которая управляет всей BTL-механикой ваших акций.
 			От работы промоутеров до розыгрышей, обучения и лояльности — всё, что раньше требовало
 			десятков Excel-файлов и координации по телефону, WhatsApp чатах, теперь решается автоматически
 			и без ошибок.
-		</p>
+		</Paragraph>
 	</div>
 </SectionBase>
