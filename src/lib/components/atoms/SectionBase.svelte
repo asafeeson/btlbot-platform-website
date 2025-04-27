132bbc84c21606a9ca9c259c119581420381dd0b
asafeeson <g.kochergin@hotmail.com>
2025-04-27 21:04:54 +0200

    - make background prop to optional

--- src/lib/components/atoms/SectionBase.svelte
+++ src/lib/components/atoms/SectionBase.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 
-	let { children, background = '' }: { children: Snippet, background: string } = $props();
+	let { children, background }: { children: Snippet, background?: string } = $props();
 </script>
 
 <section class="flex flex-wrap items-start min-h-xl justify-between gap-10 {background} p-14 lg:px-20">
