eaa63faece4cb8ca841d92c6e40ae1c25062b524
asafeeson <g.kochergin@hotmail.com>
2025-04-27 21:51:05 +0200

    refactor: improve formatting and structure in SectionBase component

--- src/lib/components/atoms/SectionBase.svelte
+++ src/lib/components/atoms/SectionBase.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
 
-	let { children, background }: { children: Snippet, background?: string } = $props();
+	let { children, background }: { children: Snippet; background?: string } = $props();
 </script>
 
-<section class="flex flex-wrap items-start min-h-xl justify-between gap-10 {background} p-14 lg:px-20">
-    {@render children()}
+<section
+	class="min-h-xl flex flex-wrap items-start justify-between gap-10 {background} p-14 lg:px-20"
+>
+	<div
+		class="rounded-4xl border border-gray-200 px-10 py-14 shadow-lg transition-shadow hover:shadow-2xl"
+	>
+		{@render children()}
+	</div>
 </section>
-
