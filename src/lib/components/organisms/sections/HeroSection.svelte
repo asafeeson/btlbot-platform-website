3efe5f659e77d73ee1f943affa4be256e66086a1
asafeeson <g.kochergin@hotmail.com>
2025-04-27 21:05:37 +0200

    - use ButtonLink component for a tag

--- src/lib/components/organisms/sections/HeroSection.svelte
+++ src/lib/components/organisms/sections/HeroSection.svelte
@@ -1,19 +1,18 @@
 <script lang="ts">
+	import ButtonLink from '$lib/components/atoms/ButtonLink.svelte';
 	import SectionBase from '$lib/components/atoms/SectionBase.svelte';
 </script>
 
 <SectionBase background="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500">
-	<div class="flex max-w-xl flex-col gap-10 py-10 mt-14 text-white">
+	<div class="mt-14 flex max-w-xl flex-col gap-10 py-10 text-white">
 		<h1 class="text-6xl font-black">Готовый чат-бот для BTL и промо акций</h1>
 		<p>
 			Всё под контролем: BTLbot автоматизирует промо-акции, работу промоутеров и коммуникацию с
 			клиентами
 		</p>
-		<a href="http://" class="border-2 border-white px-5 py-4 hover:bg-amber-500 hover:text-black transition-colors"
-			>Записаться на демо</a
-		>
+		<ButtonLink url="/">Записаться на демо</ButtonLink>
 	</div>
-	<div class="flex items-end justify-center mt-14">
+	<div class="mt-14 flex items-end justify-center">
 		<img
 			src="/robot.png"
 			alt="Робот держит в руках фискальный чек Казахстан Алматы Астана"
